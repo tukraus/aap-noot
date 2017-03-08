26cd80d6a7b4ff811955092debe0b84608878aad
Thomas Kraus <thomas@sig.nl>
2017-03-08 11:55:31 +0100

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,5 +2,23 @@ function hello() {
   var a=1;
   a=2;
   a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
+  a=2;
   return true;
 }
